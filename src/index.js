bdf089e13c0c3e4a61e9ccbc96604a33e74274e7
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-07-25 19:50:24 -0600

    Created entry file for testing board functions

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { Board, Task, Project } from "./modules/taskboard";
+
+import SaveManager from "./modules/saveManager";
+
+const saveManager = new SaveManager();
+
+
+const board = new Board("My board");
+
+//testing
+// const project = new Project("AGI", "desc");
+
+// const task = new Task("Run", "desc", "low", { day: 26, month: 7, year: 2025 });
+
+
+// project.addTask(task);
+// board.addProject(project);
+
+// board.save();
+// board.delete();
+
+
+// console.log(board.boardObj);
+// board.populate();
+
+// console.log(board.boardObj);
+
+
+
+
+
+
+
+
+
+
+
