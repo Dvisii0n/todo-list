f6e9cdcb0626371cc58e182f2358e9cc1c4666ea
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-07-25 19:50:55 -0600

    Created custom logger with timestamps

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -3,15 +3,16 @@ import { format } from "date-fns";
 
 export default class Logger {
 
-    getCurrentDate() {
+    #getCurrentDate() {
         const timestamp = new TZDate(new Date(), "-07:00")
         return `${format(timestamp, "hh:mm:ss")}`
     }
 
     log(obj, msg) {
-        console.log(`${this.getCurrentDate()} - ${msg}`);
+        console.log(`${this.#getCurrentDate()} - ${msg}`);
         console.log(obj);
         console.log("---------------------------------------------")
 
     }
+
 }
