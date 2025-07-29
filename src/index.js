72bf07d8d2c0ccc08f27bd0ea0f86a89956e7c44
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-07-29 11:27:36 -0600

    Cleaned index file to only import from other files

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-import { Board, Task, Project } from "./modules/taskboard";
-
-import SaveManager from "./modules/saveManager";
-
-const saveManager = new SaveManager();
+import { BoardContainer, ProjectContainer, TaskContainer, ButtonMethods } from "./modules/ui.js";
+import "./styles.css";
 
 
-const board = new Board("My board");
-
-//testing
-// const project = new Project("AGI", "desc");
-
-// const task = new Task("Run", "desc", "low", { day: 26, month: 7, year: 2025 });
 
 
-// project.addTask(task);
-// board.addProject(project);
-
-// board.save();
-// board.delete();
 
 
-// console.log(board.boardObj);
-// board.populate();
 
-// console.log(board.boardObj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
